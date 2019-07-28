da0200d46324ce1ecfb322686e987a73e5c96a5c
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-07-28 05:34:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,6 @@
 def roll_call_dwarves(array)
-  i = 0 
-  while i < array.length 
-    puts "#{i + 1}. #{array[i]}"
-    i += 1 
+  array.each_with_index(1) do |name, i|
+    puts "#{index}. #{name}"
   end 
 end
 
