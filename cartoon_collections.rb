cbfa62d26111fc3cc1ffd3ad37f120bd586b6df9
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-07-28 05:36:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,6 @@
 def roll_call_dwarves(array)
-  array.each_with_index(1) do |name, i|
-    puts "#{index}. #{name}"
-  end 
+  array.each_with_index(1) {|name, i|
+    puts "#{index}. #{name}"}
 end
 
 
