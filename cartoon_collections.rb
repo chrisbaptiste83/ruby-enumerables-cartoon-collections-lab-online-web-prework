b7dda60c35d993ea5774d3872e3c4c2794235804
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-07-28 06:03:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 
   def summon_captain_planet(array)
-  array.collect {|name| name.captitalize + "!"} 
+  array.collect {|name| name.capitalize + "!"} 
 end
 
 def long_planeteer_calls(array) 
